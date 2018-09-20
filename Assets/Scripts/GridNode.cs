638d13181ab029c0e8ec2feb38bb3b15eea9f24c
Thex <thex-pieddroit@hotmail.com>
2018-09-20 18:35:57 +0200

    Created GridNode prefab

---
+++ Assets/Scripts/GridNode.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+using System.Collections.Generic;
+
+
+[System.Serializable]
+public class GridNode : MonoBehaviour
+{
+#region Variables (public)
+
+	public GridNode m_pNorthNode = null;
+	public GridNode m_pWestNode = null;
+	public GridNode m_pEastNode = null;
+	public GridNode m_pSouthNode = null;
+	public GridNode m_pNorthWestNode = null;
+	public GridNode m_pSouthEastNode = null;
+
+	public Vector2 m_tPosInGrid = Vector2.zero;
+
+	#endregion
+
+#region Variables (private)
+
+
+
+	#endregion
+
+
+
+}
